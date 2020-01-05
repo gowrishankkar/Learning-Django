70aef300d270d29513b9a8c3a5a8aa88fdc40212
Gowri Shankar <shankarg.kvsulur@gmail.com>
2020-01-05 17:07:38 +0530

    Done

--- Railway_Reservation/reservation/urls.py
+++ Railway_Reservation/reservation/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.reservation, name='reservation'),
@@ -8,3 +9,5 @@ urlpatterns = [
     path('reserve', views.reserve, name='reserve'),
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
